271d302b71130e8151940c1586d2bd70f31ba51a
Falbue <cyansair05@gmail.com>
2025-05-08 17:10:45 +0500

    упростил код

--- server.py
+++ server.py
@@ -2,24 +2,17 @@ from flask import Flask, render_template
 from flask_cors import CORS
 import importlib
 import sys
-LIBRARY_NAME = "timetable-api"
+import api
 
 VERSION = "1.0.1"
 print(f"Версия: {VERSION}")
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 CORS(app)
-
-try:
-    timetable_api = importlib.import_module(LIBRARY_NAME.replace("-", "_"))  # Заменяем тире на подчёркивание
-    print(f"Библиотека '{LIBRARY_NAME}' успешно импортирована!")
-    Blueprint = api.Blueprint
-    app.register_blueprint(Blueprint)
+Blueprint = api.Blueprint
+app.register_blueprint(Blueprint)
     
-except ImportError:
-    print(f"Ошибка: библиотека '{LIBRARY_NAME}' не найдена!")
-
-
+    
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
