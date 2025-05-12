73253b5c82310cf12a8eacc46c5a7e2fc620e094
falbue <cyansair05@gmail.com>
2025-05-13 00:28:08 +0500

    обновил путь до базы

--- api/use_db.py
+++ api/use_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 DB_NAME = 'database.db'
-DB_PATH = f"{DB_NAME}"
+DB_PATH = f"/var/www/timetable.falpin.ru/{DB_NAME}"
 
 def SQL_request(request, params=(), all_data=None):  # Выполнение SQL-запросов
     connect = sqlite3.connect(DB_PATH)
